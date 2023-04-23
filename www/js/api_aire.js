702dab2e013bc9ef35df6db6accdaacb3b436076
DavidLozanoHernandez <122801602+DavidLozanoHernandez@users.noreply.github.com>
2023-04-22 22:40:18 -0600

    Modulo aire terminado

---
+++ www/js/api_aire.js
@@ -0,0 +1,16 @@
+fetch('https://calm-impalas-repair-189-161-86-109.loca.lt/log')
+    .then((response) => response.json())
+    .then(data => {
+        // Obtener el contenedor de la etiqueta html
+        let temperatura = document.getElementById('temperatura');
+        let humedad = document.getElementById('humedad');
+        let radiacion = document.getElementById('radiacion');
+        // Iterar sobre el array
+        let filtro1 = data.find(element => element.id == "temperatura_aire") 
+        let filtro2 = data.find(element => element.id == "humedad_aire")
+        let filtro3 = data.find(element => element.id == "radiacion_solar_aire")
+        //imprimir en la etiqueta
+        temperatura.innerHTML = `${filtro1.value}°`;
+        humedad.innerHTML = `${filtro2.value}%`;
+        radiacion.innerHTML = `${filtro3.value} W/m²`;
+      });
