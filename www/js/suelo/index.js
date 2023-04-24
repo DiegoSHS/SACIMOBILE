0ec3ee5e3144640aea8234321e8b064ce22a2841
coby111 <sorianocoby@gmail.com>
2023-04-24 08:58:52 -0600

    Funcionalidad para mostraar los datos obtenidos de la BD modulo Suelo

---
+++ www/js/suelo/index.js
@@ -0,0 +1,61 @@
+const server = 'https://saci.serveo.net';
+let logsUrl
+logsUrl = `${server}/api/logs`;
+
+fetch(logsUrl)
+    .then(response => response.json())
+    .then(response => {
+        const phData = {};
+        const humedadData = {};
+        const temperaturaData = {};
+
+        response.forEach(item => {
+            switch(item.id) {
+                case "ph_suelo_s1":
+                case "ph_suelo_s2":
+                    phData[item.id] = item.value;
+                    break;
+                case "humedad_suelo_s1":
+                case "humedad_suelo_s2":
+                case "humedad_suelo_s3":
+                    humedadData[item.id] = item.value;
+                    break;
+                case "temperatura_suelo_s1":
+                case "temperatura_suelo_s2":
+                case "temperatura_suelo_s3":
+                    temperaturaData[item.id] = item.value;
+                    break;
+                default:
+                    break;
+            }
+        });
+
+        const phSum = (Number(phData['ph_suelo_s1']) + Number(phData['ph_suelo_s2'])) / 2;
+        const phAvg = phSum.toFixed(2);
+        const divPh = document.getElementById('value-PH');
+        divPh.innerHTML = phAvg + " pH" || 'No se encontraron resultados';
+
+        const humedadSum = (Number(humedadData['humedad_suelo_s1']) + Number(humedadData['humedad_suelo_s2']) + Number(humedadData['humedad_suelo_s3'])) / 3;
+        const humedadAvg = humedadSum.toFixed(2);
+        const divHume = document.getElementById('value-Hume');
+        divHume.innerHTML = humedadAvg + " %" || 'No se encontraron resultados';
+
+        const temperaturaSum = (Number(temperaturaData['temperatura_suelo_s1']) + Number(temperaturaData['temperatura_suelo_s2']) + Number(temperaturaData['temperatura_suelo_s3'])) / 3;
+        const temperaturaAvg = temperaturaSum.toFixed(2);
+        const divTemp = document.getElementById('value-Temp');
+        divTemp.innerHTML = temperaturaAvg + " °C" || 'No se encontraron resultados';
+    })
+    .catch(error => console.log(error));
+
+
+
+
+function ph() {
+    window.location.href = '../../pages/suelo/ph.html'
+}
+function humedad() {
+    window.location.href = '../../pages/suelo/humedad.html'
+}
+function temperatura() {
+    window.location.href = '../../pages/suelo/temperatura.html'
+}
---
+++ www/js/suelo/ph.js
@@ -0,0 +1,22 @@
+const server = 'https://saci.serveo.net';
+let logsUrl
+logsUrl = `${server}/api/logs`;
+sensorUrl = `${server}/api/sensors`;
+
+
+fetch(logsUrl)
+    .then(response => response.json())
+    .then(response => {
+        const data = {}; // Objeto para almacenar los valores filtrados por ID
+        response.forEach(item => {
+            if (item.id === "ph_suelo_s1" || item.id === "ph_suelo_s2") {
+                data[item.id] = item.value;
+            }
+        });
+        // Actualizar los valores de los h1 correspondientes
+        const h1_1 = document.getElementById('seccion1');
+        const h1_2 = document.getElementById('seccion2');
+        h1_1.innerHTML = data['ph_suelo_s1'] + " pH" || 'No se encontraron resultados';
+        h1_2.innerHTML = data['ph_suelo_s2'] + " pH" || 'No se encontraron resultados';
+    })
+    .catch(error => console.log(error));
