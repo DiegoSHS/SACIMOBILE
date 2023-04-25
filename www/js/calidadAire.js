9305e51ac1d02f7b89c9c0ebc42f365c7b469892
Diego Lopez Ariza <diegoawesome2017@outlook.com>
2023-04-24 23:09:04 -0600

    modificacion en modulo de calidad de aire

--- www/js/calidadAire.js
+++ www/js/calidadAire.js
@@ -1,12 +1,6 @@
 const fetchData = async () => {
     try {
-<<<<<<< HEAD
         const response = fetch('https://creepy-pink-lingerie.cyclic.app/log')
-=======
-        const response = fetch('https://creepy-pink-lingerie.cyclic.app/log/',{
-            mode:"no-cors",
-        })
->>>>>>> 29c50843238a3871d9149e2761650440b7119416
         const results = await response.json()
         console.log(results)
         return results
@@ -23,10 +17,6 @@ const splitData = async () => {
 }
 
 const showData = (data) => {
-<<<<<<< HEAD
-    const { tds, co2, intensLumina } = data
-=======
->>>>>>> 29c50843238a3871d9149e2761650440b7119416
     document.getElementById('tds').innerHTML = tds
     document.getElementById('co2').innerHTML = co2
     document.getElementById('intensLumina').innerHTML = intensLumina
