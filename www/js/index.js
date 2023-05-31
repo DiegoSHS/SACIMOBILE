9a7b6157f55c4924a4cc9efd4adda90a4eedff10
Diego Lopez Ariza <diegoawesome2017@outlook.com>
2023-05-31 11:44:28 -0600

    basic interface created
    Signed-off-by: Diego Lopez Ariza <diegoawesome2017@outlook.com>

--- www/js/index.js
+++ www/js/index.js
@@ -1,6 +1,74 @@
 import { navbar } from "./components/navbar.js"
-import { getSummary } from "./requests.js";
+import { getSummary, useState } from "./requests.js";
 const setNavbar = () => {
     document.getElementById('navbar').innerHTML = navbar()
 }
-getSummary()
+
+const setValues = (records) => {
+    const {
+        aire: { tempAire, humAire, co2, lum, tds, radiation },
+        suelo: { tempAvg, phAvg, humAvg }
+    } = records
+    document.getElementById('tempAire').innerHTML = `<i aria-hidden="true" class="fi-rr-summer icon red"></i>
+    Tempe: ${tempAire} °C`
+    document.getElementById('humAire').innerHTML = `<i aria-hidden="true" class="fi-sr-humidity icon blue"></i>
+    Humedad: %${humAire}`
+    document.getElementById('co2').innerHTML = `<i aria-hidden="true" class="fi-sr-cloud icon black"></i>
+    CO2: ${co2} ppm`
+    document.getElementById('lum').innerHTML = `<i aria-hidden="true" class="fi-sr-bulb icon yellow"></i>
+    Lum: ${lum}`
+    document.getElementById('tds').innerHTML = `<i aria-hidden="true" class="fi-sr-humidity icon brown"></i>
+    TDS: ${tds} ppm`
+    document.getElementById('radiation').innerHTML = `<i aria-hidden="true" class="fi-sr-sun icon yellow"></i>
+    ${radiation} W/m²`
+    document.getElementById('tempAvg').innerHTML = `<i aria-hidden="true" class="fi-rr-thermometer-half icon red"></i>
+    Temp: ${tempAvg} °C`
+    document.getElementById('phAvg').innerHTML = `<i aria-hidden="true" class="fi-sr-leaf icon green"></i>
+    PH: ${phAvg} Unidades`
+    document.getElementById('humAvg').innerHTML = `<i aria-hidden="true" class="fi-sr-humidity icon blue"></i>
+    Humedad: %${humAvg}`
+}
+
+const removeElement = e => {
+    e.preventDefault()
+    const id = e.target.id
+    const element = document.getElementById(id)
+    element.remove()
+}
+
+const [loading, setLoading] = useState(true)
+
+const toggleLoading = () => setLoading(!loading())
+
+const loadButtons = () => {
+    const buttons = document.querySelectorAll('.record')
+    if(loading()){
+        buttons.forEach(button => {
+            button.classList.add('loading')
+            button.addEventListener('click', removeElement)
+        })
+    }else{
+        buttons.forEach(button => {
+            button.classList.remove('loading')
+        })
+    }
+}
+
+const setSummary = async () => {
+    console.log(window.location.href)
+    getSummary().then(summary => {
+        loadButtons()
+        summary
+        setTimeout(() => {
+            toggleLoading()
+            loadButtons()
+            console.log(loading())
+            console.log(summary)
+            setValues(summary)
+        },3000)
+    }
+    )
+
+}
+
+setSummary()
--- www/js/requests.js
+++ www/js/requests.js
@@ -1,7 +1,18 @@
 export const getSummary = async() => {
-    const result = await fetch('https://saci-indol.vercel.app/api/saci/logs/summary',{
-        mode: 'cors',
-    })
+    const result = await fetch('https://saci-indol.vercel.app/api/saci/logs/summary')
     const data = await result.json()
-    console.log(data)
+    return data
+}
+/**
+ * 
+ * @param {*} initialState initial value
+ * @returns Returns a function to get a stateful value, and a function to update it
+ */
+export const useState = (initialState) => {
+    let state = initialState
+    const getState = () => state
+    const setState = (newState) => {
+        state = newState
+    }
+    return [getState, setState]
 }
