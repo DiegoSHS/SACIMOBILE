61a837cf1e23406199f2dd5dbf1720bd044ad8cb
Diego López Ariza <52292080+DiegoSHS@users.noreply.github.com>
2023-06-05 16:21:55 -0600

    turn on and off actuators added (#17)
    Signed-off-by: DiegoSHS <diegoawesome2017@outlook.com>

--- www/js/components/sensorCard.js
+++ www/js/components/sensorCard.js
@@ -1,3 +1,9 @@
+
+/**
+ * 
+ * @param {Object} sensor the sensor object
+ * @returns {String} the html of the sensor card
+ */
 const SensorCard = (sensor) => {
     return (
         `<div class="ui card">
@@ -5,15 +11,23 @@ const SensorCard = (sensor) => {
                 <div class="header">${sensor.name}</div>
                 <div class="description">${sensor.description}</div>
                 <div class="ui labels">
-                    <button class="ui mini active button">Estado:${sensor.state ? 'encendido' : 'apagado'}<button/>
-                    <div class="ui label">Modulo:${sensor.type}</div>
+                    <button class="ui mini ${sensor.state ? 'green':'red'} active button">Estado:${sensor.state ? 'encendido' : 'apagado'}</button>
+                    <div class="ui label">Modulo:${sensor.module}</div>
                 </div>
             </div>
         </div>
         `
     )
 }
-
+/**
+ * 
+ * @param {Array} sensors an array of sensor objects
+ * @returns {String} the html of the sensor cards
+ */
 export const SensorCards = (sensors) => {
-    return sensors.map(sensor => SensorCard(sensor)).join('')
+    return (
+        `<div class="ui centered cards">
+            ${sensors.map(sensor => SensorCard(sensor)).join('')}
+        </div>`
+    )
 }
--- www/js/index.js
+++ www/js/index.js
@@ -1,32 +1,8 @@
-import { getSummary, useState } from "./requests.js";
+import { getActuators, getSummary, useState } from "./requests.js";
+import { setActuatorsValues, setSummaryValues } from "./sethtml.js";
 import { setupNav, getChangedTouches, getTouches } from "./touches.js";
 
 window.screen.orientation.lock('portrait')
-
-const setValues = (records) => {
-    const {
-        aire: { tempAire, humAire, co2, lum, tds, radiation },
-        suelo: { tempAvg, phAvg, humAvg }
-    } = records
-    document.getElementById('tempAire').innerHTML = `<i aria-hidden="true" class="fi-rr-summer icon red"></i>
-    Temp: ${tempAire} °C`
-    document.getElementById('humAire').innerHTML = `<i aria-hidden="true" class="fi-sr-humidity icon blue"></i>
-    Humedad: %${humAire}`
-    document.getElementById('co2').innerHTML = `<i aria-hidden="true" class="fi-sr-cloud icon black"></i>
-    CO2: ${co2} ppm`
-    document.getElementById('lum').innerHTML = `<i aria-hidden="true" class="fi-sr-bulb icon yellow"></i>
-    Lum: ${lum}`
-    document.getElementById('tds').innerHTML = `<i aria-hidden="true" class="fi-sr-humidity icon brown"></i>
-    TDS: ${tds} ppm`
-    document.getElementById('radiation').innerHTML = `<i aria-hidden="true" class="fi-sr-sun icon yellow"></i>
-    ${radiation} W/m²`
-    document.getElementById('tempAvg').innerHTML = `<i aria-hidden="true" class="fi-rr-thermometer-half icon red"></i>
-    Temp: ${tempAvg} °C`
-    document.getElementById('phAvg').innerHTML = `<i aria-hidden="true" class="fi-sr-leaf icon green"></i>
-    PH: ${phAvg} Unidades`
-    document.getElementById('humAvg').innerHTML = `<i aria-hidden="true" class="fi-sr-humidity icon blue"></i>
-    Humedad: %${humAvg}`
-}
 
 const removeElement = e => {
     e.preventDefault()
@@ -56,12 +32,14 @@ const loadButtons = () => {
 
 const setSummary = async () => {
     loadButtons()
-    getSummary().then(summary => {
-        summary
-        toggleLoading()
-        loadButtons()
-        setValues(summary)
-    })
+    const results = await Promise.allSettled([getActuators(), getSummary()])
+    const values = results.map(({ value }) => value)
+    const [actuators, summary] = values
+    console.log(actuators, summary)
+    toggleLoading()
+    loadButtons()
+    setSummaryValues(summary)
+    setActuatorsValues(actuators)
 }
 
 window.addEventListener('touchstart', getTouches)
