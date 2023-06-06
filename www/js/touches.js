ede8cfc6d9db077d60f3098329ebc26c8b04eff7
DiegoSHS <diegoawesome2017@outlook.com>
2023-06-06 17:58:37 -0500

    new page for actuator added
    Signed-off-by: DiegoSHS <diegoawesome2017@outlook.com>

--- www/js/touches.js
+++ www/js/touches.js
@@ -5,27 +5,40 @@ const [touchPos, setTouchPos] = useState({
     x: 0,
     y: 0
 })
-
+/**
+ * Returns the x and y coordinates of the touch based on the first touch
+ * @param {TouchEvent} e touch event
+ * @returns the x and y coordinates of the touch
+ */
 export const getTouches = (e) => {
     const { clientX, clientY } = e.touches[0]
     setTouchPos({ x: clientX, y: clientY })
     return { clientX, clientY }
 }
-
+/**
+ * Calculates the difference between the first touch and the current touch and sends it to the togglePage function
+ * @param {TouchEvent} e touch event
+ * @returns coordinates of the current touch and the difference between the first touch and the current touch
+ */
 export const getChangedTouches = (e) => {
     const { clientX, clientY } = e.changedTouches[0]
-    const { x, y } = touchPos()
+    const { x } = touchPos()
     const diffX = Math.trunc(clientX - x)
-    //document.getElementById('suelo').innerHTML = diffX
     togglePage(diffX)
-    return { clientX, clientY }
+    return { clientX, clientY, diffX}
 }
-
+/**
+ * Sets the active tab to the one that was clicked
+ * @param {Event} e 
+ */
 export const setActive = (e) => {
     const element = document.getElementById(e.target.id)
     setActivetab(element)
 }
-
+/**
+ * Removes the active class from all tabs and adds it to the one that was clicked
+ * @param {HTMLElement} element 
+ */
 export const setActivetab = (element) => {
     const tabs = document.querySelectorAll('.navitem')
     tabs.forEach(tab => tab.classList.remove('active'))
